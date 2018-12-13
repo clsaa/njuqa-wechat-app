cee422ce2795ed9bda2cc7ee6b9d5e37e90d6dce
heixiaofeng <18838031320@163.com>
2018-12-13 08:46:06 +0800

    增加登录注册

--- app.js
+++ app.js
@@ -1,30 +1,77 @@
 //app.js
 App({
+  globalData: {
+    userInfo: null,
+    avatarUrl: null,
+    nickname: null,
+    openId: 0,
+  },
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
     var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
+    wx.getUserInfo({
+      success: function (res) {
+        that.globalData.avatarUrl = res.userInfo.avatarUrl
+        that.globalData.nickname = res.userInfo.nickName
+        that.globalData.userInfo = res.userInfo
+        //console.log(res.userInfo)
+      }
+    })
+    //调用登录接口
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          // 发起网络请求
+          wx.request({
+            url: 'https://njuqa.clsaa.com/v1/user/byCode',
+            method: "GET",
+            header: {
+              "Content-Type": "application/json"
+            },
+            data: {
+              code: res.code
+            },
+            success(e) {
+              console.log(e.data)
+              if(e.data['nickname'] == null){
+                wx.request({
+                  url: 'https://njuqa.clsaa.com/v1/user',
+                  method: "POST",
+                  header: {
+                    "Content-Type": "application/json"
+                  },
+                  data: {
+                    avatarUrl: that.globalData.avatarUrl,
+                    nickname: that.globalData.nickname,
+                    openId:e.data['openId']
+                  }
+                })
+                //console.log(that.globalData.avatarUrl)
+                //console.log(that.globalData.nickname)
+              }
             }
           })
+        } else {
+          console.log('登录失败！' + res.errMsg)
         }
+      }
       })
-    }
   },
-  globalData:{
-    userInfo:null
+  getUserInfo:function(cb){
+    //var that = this
+    typeof cb == "function" && cb(this.globalData.userInfo)
+    // if(this.globalData.userInfo){
+    //   typeof cb == "function" && cb(this.globalData.userInfo)
+    // }else{
+    //   wx.getUserInfo({
+    //     success: function (res) {
+    //         that.globalData.userInfo = res.userInfo
+    //         typeof cb == "function" && cb(that.globalData.userInfo)
+    //     }
+    //   })
+    // }
   }
 })
