f27623ab6144d70bbe03a576981d5750362fe950
unknown <unknown@example.com>
2018-12-14 10:08:03 +0800

    测试读取问题表
    增加了内容和测试按钮

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,13 +7,17 @@ Page({
     feed: [],
     feed_length: 0
   },
+  openId: app.globalData.openId,
+  userInfo: {},
+  nickName: "",
+  textData: 'before',
   //事件处理函数
-  bindItemTap: function() {
+  bindItemTap: function () {
     wx.navigateTo({
       url: '../answer/answer'
     })
   },
-  bindQueTap: function() {
+  bindQueTap: function () {
     wx.navigateTo({
       url: '../question/question'
     })
@@ -28,36 +32,103 @@ Page({
     wx.showNavigationBarLoading()
     this.refresh();
     console.log("upper");
-    setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
+    setTimeout(function () { wx.hideNavigationBarLoading(); wx.stopPullDownRefresh(); }, 2000);
   },
   lower: function (e) {
     wx.showNavigationBarLoading();
     var that = this;
-    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
+    setTimeout(function () { wx.hideNavigationBarLoading(); that.nextLoad(); }, 1000);
     console.log("lower")
   },
- 
-  //网络请求数据, 实现首页刷新
-  refresh: function(){
-    var index_api = '';
-    util.getData(index_api)
-        .then(function(data){
-          //this.setData({
-          //
-          //});
-          console.log(data);
+
+  RequestData: function () {
+    var that = this;
+    wx.request({
+      url: 'https://njuqa.clsaa.com/v1/user/1/answer/question/all/',
+      //data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {}, // 设置请求的 header 默认是application/json
+      success: function (res) {
+        // 操作json数据
+        //var userInfo = [];
+        app.getUserInfo(function (userInfo) {
+          //更新数据
+          that.setData({
+            userInfo: userInfo
+          })
+        })
+        //console.log(userInfo);
+        console.log("request data");
+        console.log(res.data);
+        var d = res.data;
+
+        var text = res.data;
+        that.setData({
+          feed: text,
+          feed_length: text.length
         });
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+  //网络请求数据, 实现首页刷新
+  refresh: function () {
+    var index_api = 'https://njuqa.clsaa.com/v1/user/1/answer/question/all/';
+    consol.log("index/refresh_function");
+    util.getData(index_api)
+      .then(function (data) {
+        this.setData({
+
+        });
+        console.log(data);
+      });
   },
 
   //使用数据实现刷新效果
-  getData: function(){
-    var feed = [1,2,3];
+  getData: function () {
+    //var feed = [];
+    var that = this;
+    console.log(app.globalData.openId);
+    console.log("dsfadfas")
+    wx.request({
+      url: 'https://njuqa.clsaa.com/v1/user/1/answer/question/all/',
+      //data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {}, // 设置请求的 header 默认是application/json
+      success: function (res) {
+        // 操作json数据
+
+        console.log("request data");
+        console.log(res.data);
+        var d = res.data;
+        var text = res.data;
+        that.setData({
+          feed: text,
+          feed_length: text.length,
+          openId: app.globalData.openId,
+          nickName: app.globalData.nickname
+        });
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+
     console.log("loaddata");
     //var feed_data = feed.data;
+    /*
     this.setData({
       feed:feed,
       feed_length: feed.length
-    });
+    });*/
   }
 
 })
