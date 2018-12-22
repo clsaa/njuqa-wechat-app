6f3d6e0dea6fde88b8a3d9435ec64d78a1bcf0e8
heixiaofeng <18838031320@163.com>
2018-12-22 15:38:18 +0800

    授权登录

--- pages/login/login.js
+++ pages/login/login.js
@@ -12,8 +12,30 @@ Page({
         if (res.authSetting['scope.userInfo']) {
           wx.getUserInfo({
             success: function (res) {
+              console.log("unload success")
               //从数据库获取用户信息
-              that.queryUsreInfo();
+              wx.request({
+                url: 'https://njuqa.clsaa.com/v1/user/byCode',
+                method: "GET",
+                header: {
+                  "Content-Type": "application/json"
+                },
+                data: {
+                  code: getApp().globalData.code
+                },
+                success: function (res) {
+                  console.log("res...")
+                  console.log(res.data);
+                  getApp().globalData.openId = res.data.openId
+                  getApp().globalData.userInfos = res.data;
+                  getApp().globalData.avatarUrl = res.data.avatarUrl
+                  getApp().globalData.nickname = res.data.nickname
+                  if (res.data['identity'] == "ADMIN") {
+                    getApp().globalData.isAdmin = true
+                    console.log("right")
+                  }
+                }
+              })
               //用户已经授权过
               wx.switchTab({
                 url: '../index/index'
@@ -25,36 +47,69 @@ Page({
     })
   },
   bindGetUserInfo: function (e) {
+    console.log("e.detail")
+    console.log(e.detail)
+    var that = this
+    //从数据库获取用户信息
     if (e.detail.userInfo) {
-      //用户按了允许授权按钮
-      var that = this;
-      //插入登录的用户的相关信息到数据库
       wx.request({
-        url: 'https://njuqa.clsaa.com/v1/user',
-        method: "POST",
+        url: 'https://njuqa.clsaa.com/v1/user/byCode',
+        method: "GET",
         header: {
           "Content-Type": "application/json"
         },
         data: {
-          avatarUrl: e.detail.userInfo.avatarUrl,
-          nickname: e.detail.userInfo.nickname,
-          openId: e.detail.userInfo.openId
+          code: getApp().globalData.code
         },
         success: function (res) {
-          //从数据库获取用户信息
-          console.log("login success")
-          console.log(res)
-          that.queryUsreInfo();
-          getApp().globalData.avatarUrl = e.detail.userInfo.avatarUrl
-          getApp().globalData.nickname = e.detail.userInfo.nickName
-         
-          console.log("插入小程序登录用户信息成功！");
+          console.log("res...")
+          console.log(res.data);
+          getApp().globalData.openId = res.data.openId
+          getApp().globalData.userInfos = res.data;
+          getApp().globalData.avatarUrl = res.data.avatarUrl
+          getApp().globalData.nickname = res.data.nickname
+          if (res.data['identity'] == "ADMIN") {
+            getApp().globalData.isAdmin = true
+            console.log("right")
+          }
+          if(res.data.id == null){
+            //用户按了允许授权按钮
+            //插入登录的用户的相关信息到数据库
+            wx.request({
+              url: 'https://njuqa.clsaa.com/v1/user',
+              method: "POST",
+              header: {
+                "Content-Type": "application/json"
+              },
+              data: {
+                avatarUrl: e.detail.userInfo.avatarUrl,
+                nickname: e.detail.userInfo.nickName,
+                openId: getApp().globalData.openId
+              },
+              success: function (res) {
+                //从数据库获取用户信息
+                console.log("login success")
+                console.log(res)
+               
+                getApp().globalData.userInfos = res.data
+                getApp().globalData.avatarUrl = res.data.avatarUrl
+                getApp().globalData.nickname = res.data.nickname
+                console.log("插入小程序登录用户信息成功！");
+                //授权成功后，跳转进入小程序首页
+                wx.switchTab({
+                  url: '../index/index'
+                })
+              }
+            });
+
+          }else{
+            wx.switchTab({
+              url: '../index/index'
+            })
+          }
         }
-      });
-      //授权成功后，跳转进入小程序首页
-      wx.switchTab({
-        url: '../index/index'
       })
+      
     } else {
       //用户按了拒绝按钮
       wx.showModal({
@@ -69,29 +124,6 @@ Page({
         }
       })
     }
-  },
-  //获取用户信息接口
-  queryUsreInfo: function () {
-    wx.request({
-      url: 'https://njuqa.clsaa.com/v1/user/byCode',
-      method: "GET",
-      header: {
-        "Content-Type": "application/json"
-      },
-      data: {
-        code: getApp().globalData.code
-      },
-      success: function (res) {
-        console.log(res.data);
-        getApp().globalData.userInfos = res.data;
-        getApp().globalData.avatarUrl = res.data.avatarUrl
-        getApp().globalData.nickname = res.data.nickname
-        if (res.data['identity'] == "ADMIN") {
-          getApp().globalData.isAdmin = true
-          console.log("right")
-        }
-      }
-    })
   },
 
 })
